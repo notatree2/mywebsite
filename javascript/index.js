12a494ac7d5b61c2f03866e3b118044fb1642c71
CrOS Man <juliswater@gmail.com>
2024-05-24 19:30:52 -0400

    2 lines

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,2 @@
 document.title("notacupofmilk's website")
-
 console.log("yo welcome to the console how u been today, checking up on my website? pretty cool u can do that btw i aint stopping u lol")
