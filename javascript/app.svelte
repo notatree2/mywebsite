c1efafd7927ddb369e87646cee6463ee4a36ebd0
CrOS Man <juliswater@gmail.com>
2024-05-24 19:32:38 -0400

    got rid of svelte didn't need it 1 more line of javascript! a line with a comment on it!!!

--- javascript/app.svelte
+++
@@ -1,1 +0,0 @@
-/* to be used later */
--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,3 @@
+// add svelte file later 5/24/24
 document.title("notacupofmilk's website")
 console.log("yo welcome to the console how u been today, checking up on my website? pretty cool u can do that btw i aint stopping u lol")
