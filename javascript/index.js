a12e3258118b83be10891875456bd5f33d81b276
CrOS Man <juliswater@gmail.com>
2024-05-24 20:37:23 -0400

    tried to modified javascript?

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,15 @@
-// add svelte file later 5/24/24
-document.title("notacupofmilk's website")
+
 console.log("yo welcome to the console how u been today, checking up on my website? pretty cool u can do that btw i aint stopping u lol")
+
+const allA = document.getElementsByTagName("a")
+
+for(let a of allA) {
+    a.style.justifyContent = "space-evenly"
+    a.style.color = "blue"
+}
+
+const allP = document.getElementsByTagName("p")
+
+allP.forEach(f => {
+    f.style.color = "blue"
+})
