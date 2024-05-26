99a35a30055a20462299829180e46cb538d39e92
CrOS Man <juliswater@gmail.com>
2024-05-26 12:54:36 -0400

    (these are long so i can give you detailed updates) Fixed javascript (added some more) Added a list of facts about me made the links at the very top have a font and are blue now

--- javascript/e.js
+++ javascript/e.js
@@ -1,11 +1,15 @@
 
 console.log("yo welcome to the console how u been today, checking up on my website? pretty cool u can do that btw i aint stopping u lol")
 
-const allA = document.getElementsByTagName("a")
+const allA = document.getElementsByTagName("li")
+
 
 for(let a of allA) {
-    a.style.justifyContent = "space-evenly"
-    a.style.color = "blue"
+    a.style.color = "#6888BE"
 }
 
+const NavBar = document.getElementsByClassName("navbar")
 
+const f = document.getElementById("f")
+
+f.style.color = "#4470AD"
